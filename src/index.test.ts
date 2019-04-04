d1121cc60d3ed4f266479f0511aa646d6148575c
Toru Kobayashi <koba0004@gmail.com>
2019-04-05 00:19:13 +0900

    feat: add ok function

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,15 @@
 import assert from 'assert';
-import { equal } from './';
+import { equal, ok } from './';
 
 describe('equal', () => {
   it('should pass', () => {
     assert.equal(equal('foo', 'foo'), true);
   });
 });
+
+describe('ok', () => {
+    it('should pass', () => {
+      assert.equal(ok(true), true);
+      assert.equal(ok(false), false);
+    });
+  });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export const equal = (a: unknown, b: unknown): boolean => {
   return a === b;
 }
+
+export const ok = (a: unknown): boolean => {
+    return a === true;
+  }
